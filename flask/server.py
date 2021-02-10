5c380294fe62608770820f7ae074a4a4d757979c
teperwoman <teperwoman89@gmail.com>
2021-02-10 18:09:24 +0200

    change images name

--- flask/server.py
+++ flask/server.py
@@ -18,5 +18,5 @@ def todo():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9091), debug=True)
 
